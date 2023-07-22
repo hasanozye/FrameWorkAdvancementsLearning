0488b926f3144d376c3d4f354c96bd3818e9b749
Hasan Özyer <hasanozyer06@gmail.com>
2023-07-22 12:23:05 +0300

    "Introducing the super-duper BaseTest class, equipped with a secret sauce 🌟 of Selenium sorcery and ninja-like precision 🥷 for clicking, typing, and hovering like a pro! Now you can test like a boss 🕶️ and conquer bugs with a touch of pizzazz ✨🎉!"

--- src/test/java/com/guidersoft/utils/BaseTest.java
+++ src/test/java/com/guidersoft/utils/BaseTest.java
@@ -1,2 +1,46 @@
-package com.guidersoft.utils;public class BaseTest {
+package com.guidersoft.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    protected void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+    protected void click(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+
+    }
+
+    protected void sendKeys(By locator, String text) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).sendKeys(text);
+    }
+
+    protected void sendKeys(WebElement element, String text) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).sendKeys(text);
+    }
+
+    protected void hover(WebElement element) {
+        new Actions(driver).moveToElement(element).perform();
+    }
+
+    protected void actionWait(long milis) {
+        new Actions(driver).pause(Duration.ofMillis(milis)).perform();
+    }
+
+    protected void waitForPresence(By locator){
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+
 }
