bf14793f2eb411853e4a9de91ec6133852936c50
Hasan Özyer <hasanozyer06@gmail.com>
2023-07-22 13:26:29 +0300

    "🔎 Unlock the secrets of Locators: Behold the magical interface that holds the keys to the web elements! 🗝️✨ Journey to the realm of Hasan's Locators and harness their power to traverse the web of wonders! 🌐🌟 From the mystical LoginLink to the enchanted SearchButton, these mystical By locators shall guide you on your web testing quests! 🚀🔮 Soak in the brilliance of Locators and unlock the true potential of your web tests with Hasan! 🌈🧙‍♂️ Embrace the power of Locators and witness the magic they bring to your testing realm! 🪄🌟"

--- src/test/java/com/guidersoft/utils/Locators.java
+++ src/test/java/com/guidersoft/utils/Locators.java
@@ -1,2 +1,19 @@
-package com.guidersoft.utils;public interface Locators {
+package com.guidersoft.utils;
+
+import org.openqa.selenium.By;
+
+public interface Locators {
+
+    String url = "https://opencart.abstracta.us/";
+    By lLoginLink = By.xpath("//div[@id='top-links']//a[text()='Login']");
+    By lMyAccount = By.xpath("//div[@id='top-links']//a[contains(.,'My Account')]");
+    By lLoginForUsername = By.id("input-email");
+    By lLoginFormPassword = By.cssSelector("#input-password");
+    By lLoginFormSubmitButton = By.cssSelector("input[value='Login']");
+//    By lMyAccountText = By.xpath("//div[@id='content']/h2[text()='My Account']");
+    By lLogOutlink = By.xpath("//div[@id='top-links']//a[text()='Logout']");
+    By lSearchInput = By.cssSelector("#search > input");
+    By lSearchButton = By.cssSelector("#search button");
+    By lListedProducts = By.cssSelector(".product-thumb");
+    By lMainMenuLinks = By.cssSelector(".nav.navbar-nav > li");
 }
