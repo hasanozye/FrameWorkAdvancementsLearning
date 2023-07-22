5ac051d191e9c3a7efc6018ea0f384a650f0e141
Hasan Özyer <hasanozyer06@gmail.com>
2023-07-22 13:27:39 +0300

    🧪 Unleash the Testing Saga: Join the quest to test the mighty web application! 🚀🌐 In the first test, brave warriors shall embark on a login journey through the EDGE of the web! ⚔️🛡️ Defeating the challenges, they will login and triumphantly logout! 💻✨
    In the second test, the seekers of knowledge shall venture into the world of searches! 🔍💡 Testing the power of the web's search capabilities, they shall ensure the correct number of magical 'Mac' products are revealed! 🧙‍♀️🔮
    
    But behold, the third test will unravel the mysteries of the hovering menu! 🌟🔥 Hasan's warriors will walk over the enchanted menu, exploring its sub-menus and harnessing their hidden power! 🗝️🔎
    
    Join Hasan's Testing Warriors in their epic journey to ensure the web app's might and magic! 🌈🗺️ Witness the triumphs and celebrate the success of Hasan's Tests! 🎉🎆 Let the testing saga continue in the pursuit of perfection! 🚀🧪"

--- src/test/java/com/guidersoft/test/Tests.java
+++ src/test/java/com/guidersoft/test/Tests.java
@@ -1,2 +1,52 @@
-package com.guidersoft.test;public class Tests {
+package com.guidersoft.test;
+
+import com.guidersoft.pages.HomePage;
+import com.guidersoft.utility.Browsers;
+import com.guidersoft.utility.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static com.guidersoft.utils.Locators.*;
+
+public class Tests {
+
+    WebDriver driver;
+
+    @Test
+    public void loginTest() {
+        driver = Driver.getDriver(Browsers.EDGE);
+        HomePage homePage = new HomePage(driver);
+        homePage.open(url);
+        homePage.login("hasanozyer@acme.com", "hasanozyerDeneme");
+        homePage.logout();
+        driver.quit();
+    }
+
+    @Test
+    public void searchTest() {
+        HomePage homePage = new HomePage();
+        homePage.open(url);
+        homePage.search("mac");
+        List<WebElement> list = homePage.getSearchList();
+        Assert.assertEquals(list.size(), 4);
+
+        List<WebElement> listMacBook = homePage.getSearchList("MacBook");
+        Assert.assertEquals(listMacBook.size(), 3);
+        Driver.getDriver().quit();
+
+
+    }
+
+    @Test
+    public void hoverMenu(){
+        HomePage homePage = new HomePage();
+        homePage.open(url);
+        homePage.walkOverMenu();
+        Driver.getDriver().quit();
+    }
+
 }
